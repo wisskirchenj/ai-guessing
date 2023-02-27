43c3a5b8401ae368416bc84f3bf3a79e04d264f5
wisskirchenj <juergen.wisskirchen@cofinpro.de>
2023-02-27 22:34:52 +0100

    Make gradlew executable

--- gradlew.bat
+++ gradlew.bat
@@ -15,17 +15,18 @@
 @rem
 
 @if "%DEBUG%"=="" @echo off
-@rem \#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#
+@rem ##########################################################################
 @rem
 @rem  Gradle startup script for Windows
 @rem
-@rem \#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#
+@rem ##########################################################################
 
 @rem Set local scope for the variables with windows NT shell
 if "%OS%"=="Windows_NT" setlocal
 
 set DIRNAME=%~dp0
 if "%DIRNAME%"=="" set DIRNAME=.
+@rem This is normally unused
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 
